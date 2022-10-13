d20806f33982dd5295681b2a9fc140690001f81b
opi2909 <o.p.ivanova@outlook.com>
2022-10-13 15:16:25 +0300

    Lesson4.2

--- src/main/java/org/example/Triangle.java
+++ src/main/java/org/example/Triangle.java
@@ -1,13 +1,13 @@
 package org.example;
 
 public class Triangle {
-    public static void main(String[] args) {
+    public static void main(String[] args)  {
         Aria(3, 4, 5);
 
     }
 
-    public static double Aria(int a, int b, int c) throws MyException {
-        if (a<=0 || b<=0 || c <=0) throw new MyException ("Текст моей ошибки");
+    public static double Aria(int a, int b, int c) {
+        //if (a<=0 || b<=0 || c <=0) throw new MyException ("Не верное значение сторон");
         double p = (a + b + c) / 2;
         double S = Math.sqrt(p * (p - a) * (p - b) * (p - c));
         System.out.println(S);
--- src/test/java/org/example/TriangleTest.java
+++ src/test/java/org/example/TriangleTest.java
@@ -3,14 +3,27 @@ package org.example;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TriangleTest {
 
     @Test
-    @DisplayName("Позитивный тест 1")
-    public void TriangleTest() {
+    @DisplayName("Стороны корректны")
+    public void Triangle1Test() {
 
-        assertTrue( true );
+        assertTrue(Triangle.Aria(3,4,5) > 0 );
+    }
+    @Test
+    @DisplayName("Одна из сторон равна 0")
+    public void Triangle2Test() {
+
+        assertFalse(Triangle.Aria(0,4,5) > 0 );
+    }
+    @Test
+    @DisplayName("Одна из сторон имеет отрицательное значение")
+    public void Triangle3Test() {
+
+        assertFalse(Triangle.Aria(-1,4,5) > 0 );
     }
 }
